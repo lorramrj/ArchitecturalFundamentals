de46f4dcc80c58a52712da106dd3a9e0940afb6b
Lorram Nascimento <lorramrj@gmail.com>
2022-04-30 16:57:51 -0300

    Class Funcionario / Processo

---
+++ 01 - OOP/OOP/02_ Pilares_OOP/02_Heranca.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Funcionario : Pessoa
+    {
+        public DateTime DataAdmissao { get; set; }
+        public string Registro { get; set; }
+    }
+
+    public class Processo
+    {
+        public void Execucao()
+        {
+            var funcionario = new Funcionario()
+            {
+                Nome = "Lorram N de Oliveira",
+                DataNascimento = Convert.ToDateTime("1990/20/12"),
+                DataAdmissao = DateTime.Now,
+                Registro = "0121245"
+            };
+
+            funcionario.CalcularIdade();
+        }
+    }
+}
+
