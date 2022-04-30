cd6a00f98814a92d6fff4b129ad1c9672d6a3a88
Lorram Nascimento <lorramrj@gmail.com>
2022-04-30 16:35:23 -0300

    State and Comportament

---
+++ 01 - OOP/OOP/01 - ClassexObjeto/Casa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Casa
+    {
+        public int TamanhoM2 { get; set; }
+        public int Andares { get; set; }
+        public int Valor { get; set; }
+        public int NumeroVagas { get; set; }
+    }
+
+    public class Objeto
+    {
+        public Objeto()
+        {
+            var casa = new Casa()
+            {
+                TamanhoM2 = 100,
+                Andares = 2,
+                Valor = 100000,
+                NumeroVagas = 3
+            };
+        }
+    }
+}
