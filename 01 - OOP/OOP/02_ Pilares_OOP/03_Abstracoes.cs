29c343ac00a7dfabf7b8042e99c898804c70591f
Lorram Nascimento <lorramrj@gmail.com>
2022-04-30 17:25:52 -0300

    Class Abstracoes

---
+++ 01 - OOP/OOP/02_ Pilares_OOP/03_Abstracoes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public abstract class Eletrodomestico
+    {
+        private readonly string _nome;
+        private readonly int _voltagem;
+
+        protected Eletrodomestico(string nome, int voltagem)
+        {
+            _nome = nome;
+            _voltagem = voltagem;
+        }
+
+        public abstract void Ligar();
+        public abstract void Desligar();
+    }
+}
